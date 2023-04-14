df16481a754f444abcee40d4f3c87dcdc30689db
Ali Romero <aliyeketaz@gmail.com>
2023-04-14 13:03:56 +0430

    added new files

---
+++ arsipanel/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      body: ["BYekan"],
+      display: ["BYekan"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
